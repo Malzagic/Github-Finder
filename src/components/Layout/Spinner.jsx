1b453be1a0271deed9be5925e60b4d848c6f7779
Malzagic <przemek2142@gmail.com>
2022-09-03 13:03:57 +0200

    change few things

--- src/components/Layout/Spinner.jsx
+++ src/components/Layout/Spinner.jsx
@@ -4,7 +4,7 @@ import spinner from './assets/spinner.gif';
 function Spinner() {
   return (
     <div className='w-100 mt-20'>
-      <img className='text-center mx-auto' width={180} src={spinner} alt="Loading..." />
+      <img className='text-center mx-auto' width={180} src={spinner} />
     </div>
   )
 }
